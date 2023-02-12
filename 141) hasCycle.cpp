feb27925ee09dfac6a04d640f46f077f4de12d3f
Dipanshuofficial <114001896+Dipanshuofficial@users.noreply.github.com>
2023-02-12 20:40:13 +0530

    Create 141) hasCycle.cpp

---
+++ 141) hasCycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        int count=0;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(fast==NULL){
+            return 0;
+        }
+        if(fast->next==NULL){
+            return 0;
+        }
+        return 1;
+    }
+};
