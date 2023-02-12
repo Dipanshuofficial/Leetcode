6d0e460f1ba94b4ab15946a6d3e6b021f5e79039
Dipanshuofficial <114001896+Dipanshuofficial@users.noreply.github.com>
2023-02-12 20:25:29 +0530

    Create 28) strStr.cpp

---
+++ 28) strStr.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        // unordered_map<string,int> mp;
+        int x=needle.size();
+        return haystack.find(needle);
+        return -1;
+    }
+};
